abfbca8819567844a705e0cd8bd51705d1b61b81
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-18 00:31:59 -0400

    feature: initialize messages module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NotionService } from './notion/notion.service';
 import { NotionController } from './notion/notion.controller';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MessagesModule],
   controllers: [NotionController],
   providers: [NotionService],
 })
